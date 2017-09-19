94ded948307b1977c6c81d8391b81844ebde216f
FlavioCruz <flaviocoutinho.nave@gmail.com>
2017-09-18 22:42:46 -0300

    Zoom de camera do mapa sendo implementado

--- New Unity Project 1/Assets/CSs/MapController.cs
+++ New Unity Project 1/Assets/CSs/MapController.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class MapController : MonoBehaviour
 {
-
+    public Vector3 defaultCameraPosition = new Vector3(202, -99.4f, 155.6f);
+    public int defaultCameraSize = 8;
+    public bool isZoomActivated = false;
+    public Camera camera;
     // Use this for initialization
     void Start()
     {
@@ -15,5 +18,25 @@ public class MapController : MonoBehaviour
     void Update()
     {
 
+    }
+
+    void OnMouseDown()
+    {
+        Debug.Log("Mouse");
+        
+        var mouse = Input.mousePosition;
+        Debug.Log(mouse);
+        if (!isZoomActivated)
+        {
+            camera.orthographicSize = 2;
+            transform.position = new Vector3 (Camera.main.ScreenToWorldPoint(mouse).x, Camera.main.ScreenToWorldPoint(mouse).y, transform.position.z);
+            isZoomActivated = true;
+        }
+        else
+        {
+            camera.orthographicSize = defaultCameraSize;
+            //transform.position = defaultCameraPosition;
+            isZoomActivated = false;
+        }
     }
 }
