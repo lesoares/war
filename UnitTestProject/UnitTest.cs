ca0aebf772d713dd54adba91003c77ecea3fedbf
Erick Arruzzo <erick.rezende@flysistemas.com>
2017-12-07 21:53:49 -0200

    Teste unitário do metodo checkObjective da classe GameController

---
+++ UnitTestProject/UnitTest.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject
+{
+    [TestClass]
+    public class UnitTest
+    {
+        [TestMethod]
+        public void TestMethodCheckObjective()
+        {
+            //Vez do player e ele não possui mais de 20 territorios
+            Assert.AreEqual(TerritoryControllerTest.checkObjective(true, 15, 20), false);
+
+            //Vez do player e ele possui 20 ou mais territorios
+            Assert.AreEqual(TerritoryControllerTest.checkObjective(true, 20, 15), true);
+
+            //Vez da IA e ela possui menos de 20 territorios
+            Assert.AreEqual(TerritoryControllerTest.checkObjective(false, 20, 15), false);
+
+            //Vez da IA e ela possui 20 ou mais territorios
+            Assert.AreEqual(TerritoryControllerTest.checkObjective(false, 15, 20), true);
+        }
+    }
+}
